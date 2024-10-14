79e12179d712610bbd9fbbb4326110829aa11029
Virginia Majuelos <virginia.majuelos@gmail.com>
2024-10-14 15:58:43 +0200

    config errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-export { Button } from "@atoms/Button/Button";
-export { Card } from "@atoms/Card/Card";
-export { PillIcon } from "@atoms/PillIcon/PillIcon";
-export { PillText } from "@atoms/PillText/PillText";
-export { Spinner } from "@atoms/Spinner/Spinner";
-export { Accordion } from "@molecules/Accordion/Accordion";
-export { Header } from "@molecules/Header/Header";
-export { Modal } from "@molecules/Modal/Modal";
-
 import "./styles/global.css";
 import "./styles/reset.css";
 import "./styles/variables.css";
+
+export { Button } from "../src/components/Atoms/Button/Button";
+export { Card } from "../src/components/Atoms//Card/Card";
+export { PillIcon } from "../src/components/Atoms//PillIcon/PillIcon";
+export { PillText } from "../src/components/Atoms//PillText/PillText";
+export { Spinner } from "../src/components/Atoms//Spinner/Spinner";
+export { Accordion } from "../src/components/Molecules/Accordion/Accordion";
+export { Header } from "../src/components/Molecules/Header/Header";
+export { Modal } from "../src/components/Molecules/Modal/Modal";
+
+
--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,6 @@ export default defineConfig({
         },
       }
     : {
-        outDir: "dist-app", 
+        outDir: "dist-app",
       },
 });
