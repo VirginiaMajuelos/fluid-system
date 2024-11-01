1e1e312a3ba08be9fc7af0c90e614ccc73349f98
Virginia Majuelos <virginia.majuelos@gmail.com>
2024-11-01 09:32:33 +0100

    Update .gitignore to exclude .storybook, dist-app, and dist-lib folders

--- storybook-static/assets/preview-BXdunPe6.js
+++
@@ -1,1 +0,0 @@
-const a={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},backgrounds:{default:"light",values:[{name:"light",value:"#f2f2f2"},{name:"dark",value:"#333"}]},fontSize:{default:"font-family",value:"Quicksand"}}};export{a as default};
